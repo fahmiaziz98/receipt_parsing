082ac84f1c20646a295d66a18e41a269d1c58d89
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2024-06-20 23:25:14 +0700

    add service gdrive

---
+++ Gemini-Vision/gdrive/service.py
@@ -0,0 +1,27 @@
+import os
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+class GoogleDriveService:
+
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+    def __init__(self):
+        # the directory where your credentials are stored
+        base_path = "/home/fahmiaziz/project_py/gcp"
+        
+        # The name of the file containing your credentials
+        credential_path = os.path.join(base_path, "gdrive.json")
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
+
+    def build(self):
+        
+        # Get credentials into the desired format
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            os.getenv("GOOGLE_APPLICATION_CREDENTIALS"), self.SCOPES
+        )  
+        
+        # Set up the Gdrive service object
+        service = build("drive", "v3", credentials=creds, cache_discovery=False)
+
+        return service
